12299848d75e053da5cb74a68c1896d6c759d274
me <60118488+LordPhyre@users.noreply.github.com>
2025-09-30 15:13:32 -0400

    Improved comments and improved code. Also added some flexibility to allow different lottery types.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-//comment?: read file -> do thing -> spit out numbers -> user input
-
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use regex::Regex;
 use rand::prelude::*;
 use rand::distributions::WeightedIndex;
-use rand::Rng;
 
 fn main() {
-    
     // File must exist in the current path
     if let Ok(lines) = read_lines("src/LotteryUSA.txt") {
         // Consumes the iterator, returns an (Optional) String
         let regex = Regex::new(r"[0-9]+").unwrap();
-        // Vector of vectors with lottery numbers
-        let mut lottoVector = Vec::new();
+        
+        // Vector of vectors with lottery numbers. This is mainly for show and isn't used in any calculations. Maybe in the future, inter-set correlations can be made with this information.
+        let mut winning_vectors = Vec::new();
+        // A non-nested vector that will hold every occurrence of a winning number. Used in calculation)
+        let mut winning_nums = Vec::new();
         
         // We are using a counter to go through the file line-by-line and extract the lottery numbers
         let mut counter = 0;
@@ -25,44 +24,30 @@ fn main() {
             if (counter % 5) == 2 {
                 // 'm' is a 'Match', and 'as_str()' returns the matching part of the haystack.
                 // Converts would be to vector of strings to vector of ints within same line!
-		        let lotto_nums: Vec<i32> = regex.find_iter(&line).map(|m| m.as_str()).map(|s| s.parse().unwrap()).collect();
-		        // Push the extracted lottery numbers (as a vector) into another vector
-		        lottoVector.push(lotto_nums);
+                let mut lotto_nums: Vec<i32> = regex.find_iter(&line).map(|m| m.as_str()).map(|s| s.parse().unwrap()).collect();
+                
+                // Push the extracted lottery numbers into the respective variable
+                winning_vectors.push(lotto_nums.clone());
+                winning_nums.append(&mut lotto_nums)
             }
         }
-        println!("{:#?}", lottoVector);
-        
-        
-        // Create new vector that doesnt have nesting (used in calculation)
-        let mut lotto_vector_simple = Vec::new();
-        let lotvec_size = lottoVector.iter().count();
-        for n in 0..lotvec_size {
-            lotto_vector_simple.push(lottoVector[n][0]);
-            lotto_vector_simple.push(lottoVector[n][1]);
-            lotto_vector_simple.push(lottoVector[n][2]);
-            lotto_vector_simple.push(lottoVector[n][3]);
-            lotto_vector_simple.push(lottoVector[n][4]);
-        }
-        //println!("{:?}", lotto_vector_simple);
+        //println!("{:#?}", lottoVector);
+        //println!("{:?}", winning_nums);
         
         
         // Setup random number gen
-        let choices = [
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
-            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-            21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-            31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-            41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-            51, 52, 53, 54, 55, 56, 57, 58, 59, 60
-        ];
+        let lower_bound = winning_nums.iter().min().unwrap();
+        let upper_bound = winning_nums.iter().max().unwrap();
         
-        let mut weights = [0; 60];
-        for num in 1..61 {
-            // Generate weights based on count of number / total amount of numbers * 1000 (to get rid of decimals)
-            let num_count = lotto_vector_simple.iter().filter(|x| **x == num).count();
-            let total = lotto_vector_simple.iter().count();
-            let num_weight = (num_count * 1000)/total;
-            weights[(num - 1) as usize] = num_weight;
+        // Create an array to hold weights for each possible number, later used to influence random number picking. Array size is just a random big number
+        let mut weights = [0; 99];
+        
+        for num in *lower_bound..*upper_bound+1 {
+            // Generate weights based on count of number / total amount of numbers
+            let num_count = winning_nums.iter().filter(|x| **x == num).count();
+            let num_weight = num_count as f32 / winning_nums.len() as f32;
+            // Final weight value must be multiplied by 10000 in order to avoid being truncated to zero
+            weights[(num - 1) as usize] = (num_weight * 10000.0) as usize;
             
             println!("number: {:?}", num);
             println!("appeared: {:?} times", num_count);
@@ -77,16 +62,14 @@ fn main() {
         
         let mut generated_nums = [0, 0, 0, 0, 0];
         for n in 0..5 {
-            let mut random_number = choices[dist.sample(&mut rng)];
+            let random_number = winning_nums[dist.sample(&mut rng)];
             generated_nums[n as usize] = random_number;
             weights[(random_number - 1) as usize] = 0;
         }
         generated_nums.sort();
         
-        // Generate powerball
-        let powerball = rand::thread_rng().gen_range(1..5);
         // Display winning numbers
-        println!("{:?}, {}", generated_nums, powerball);
+        println!("{:?}", generated_nums);
         
         
     // If file not found
